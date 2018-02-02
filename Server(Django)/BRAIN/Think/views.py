c92a4003109f1318c725e45ed67ac7a28975f47f
Aditya K <adk@Adityas-MacBook-Pro.local>
2018-02-02 22:46:57 +0400

    Exception handling.

--- Server(Django)/BRAIN/Think/views.py
+++ Server(Django)/BRAIN/Think/views.py
@@ -18,7 +18,11 @@ def getPersonInfo(request):
 		print ("Obtained an image.")
 
 		rollno = getPersonFromImage(b64Image)
+		print ("Obtained the roll number.")
+
 		data = getPersonDetails(rollno)
+		print ("Obtained the person's data ..")
+
 		return HttpResponse(json.dumps(data))
 
 	except Exception as e:
@@ -35,17 +39,24 @@ def getPersonFromImage(b64_image):
 
 	# Convert the base 64 encoded string into an image file and save it.
 	convertB64toImageAndSave(b64_image, image_file_name)
+	print ("Saved the image.")
 
 	# Now open the file and get the encodings as a list.
 	unknown_encodings = getEncodingsFromImageFile(image_file_name)
+	print ("Got the unknown image's encodings.")
 
 	# Get the known encodigns.
 	known_encodings = getKnownEncodings(CSV_filename)
+	print ("Got the known encodings.")
 
 	# Now compare the encodings to get the rollnumber ( adding 1210314801 because
 	# the compare function returns the index of the encoding in the known encodings list
 	# that best matches the unknown encoding ).
-	possible_roll_number = 1210314802 + compare(known_encodings, unknown_encodings)
+	delta = compare(known_encodings, unknown_encodings)
+	print ("Got the delta = " + delta)
+
+	possible_roll_number = 1210314802 + delta
+	print ("Possible roll number - " + possible_roll_number)
 
 	return possible_roll_number
 
@@ -72,8 +83,8 @@ def getKnownEncodings():
 		with open('Encodings.csv', 'r') as c:
 			reader = csv.reader(c, delimiter=';', dialect='excel')
 			for r in reader:
-				#rolls.append(int(r[0]))
 				encodings.append([float(r[x]) for x in range(1, len(r))])
+		print ("Loaded the known encodings list from the csv. The size is " + len(list))
 	except Exception as e:
 		print ("Exception in getKnownEncodings " + e)
 
@@ -87,13 +98,15 @@ def convertB64toImageAndSave(b64_string, image_file_name):
 		#image_file_name is the file name where the image is stored
 		#imgdata is returned
 		imgdata = base64.b64decode(b64_string)
+		print ("Decoded the unknown image.")
+
 		image_file_name = 'some_image.jpg'
 		with open('image_file_name','wb') as fo:
 			fo.write(imgdata)
-		return imgdata
+		print ("Saved the unknown image")
+
 	except Exception as e:
 		print ("Exception in convertB64toImageAndSave " + e)
-	return None
 
 # Open the given image file and extract the 128 encodings and return them.
 def getEncodingsFromImageFile(image_file_name):
@@ -101,17 +114,24 @@ def getEncodingsFromImageFile(image_file_name):
 	try:
 		#Just loading the image and encoding the Unknown image values
 		unknown_image = face_recognition.load_image_file("image_file_name")
+		print ("Loaded the unknown image.")
+
 		unknown_encodings = face_recognition.face_encodings(unknown_image)[0]
+		print ("Obtained the unknown encodings")
 		return unknown_encodings
+
 	except Exception as e:
 		print ("Exception in getEncodingsFromImageFile " + e)
-	return None
+
+	return []
 
 # Compare the face with the given list of encodings and return the possible
 # entry in the known encodings. Return -1 if nothing's been found.
 def compare(known_encodings, unknown_encodings):
 	try:
 		results = fr.compare_faces([known_encodings], unknown_encodings, tolerance = tol)
+		print ("Compared faces. Got results of len " + len(results))
+
 		for m in range(0, len(results)):
 			if results[m]:
 				return m
