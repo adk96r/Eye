929276aeddd19d42c83ac5abba29f6fae256af66
Aditya K <adk96r@gmail.com>
2018-02-02 17:48:11 +0400

    Added comparison code.

--- Server(Django)/BRAIN/Think/views.py
+++ Server(Django)/BRAIN/Think/views.py
@@ -12,7 +12,6 @@ import face_recognition
 @csrf_exempt
 @require_http_methods(["GET", "POST"])
 def getPersonInfo(request):
-
 	try:
 		data = request.POST
 		b64Image = data["ImageData"]
@@ -33,7 +32,7 @@ def getPersonInfo(request):
 def getPersonFromImage(b64_image):
 
 	image_file_name = "image.jpg"
-	
+
 	# Convert the base 64 encoded string into an image file and save it.
 	convertB64toImageAndSave(b64_image, image_file_name)
 
@@ -46,7 +45,7 @@ def getPersonFromImage(b64_image):
 	# Now compare the encodings to get the rollnumber ( adding 1210314801 because
 	# the compare function returns the index of the encoding in the known encodings list
 	# that best matches the unknown encoding ).
-	possible_roll_number = 1210314801 + compare(known_encodings, unknown_encodings)
+	possible_roll_number = 1210314802 + compare(known_encodings, unknown_encodings)
 
 	return possible_roll_number
 
@@ -65,14 +64,21 @@ def getPersonDetails(rollno):
 
 	return person
 
-
-# FUNCTIONS TO WRITE
+# Return the csv as a list.
+def getKnownEncodings():
+	encodings = list()
+	with open('Encodings.csv', 'r') as c:
+		reader = csv.reader(c, delimiter=';', dialect='excel')
+		for r in reader:
+			#rolls.append(int(r[0]))
+			encodings.append([float(r[x]) for x in range(1, len(r))])
+	return encodings
 
 # Take a b64 file and convert it and save it as <imageFileName>.jpg.
 def convertB64toImageAndSave(b64_string, image_file_name):
 
 	#imgdata takes the decoded base64 string into imgdata
-	#image_file_name is the file name where the image is stored 
+	#image_file_name is the file name where the image is stored
 	#imgdata is returned
 	imgdata = base64.b64decode(b64_string)
 	image_file_name = 'some_image.jpg'
@@ -81,43 +87,19 @@ def convertB64toImageAndSave(b64_string, image_file_name):
 	return imgdata
 
 
-	
-
-
 # Open the given image file and extract the 128 encodings and return them.
-def getEncodingsFromImageFile(image_file_name)
+def getEncodingsFromImageFile(image_file_name):
 
-#Just loading the image and encoding the Unknown image values
-unknown_image = face_recognition.load_image_file("image_file_name")
-unknown_encodings = face_recognition.face_encodings(unknown_image)[0]
-
-return unknown_encodings
-	
-
-
-# Open the database .CSV and return the data as a list.
-def getKnownEncodings()
-#When reading the encodings.csv file its showing IndexError 
-try:
-
-except IndexError:
-
-	
-
+	#Just loading the image and encoding the Unknown image values
+	unknown_image = face_recognition.load_image_file("image_file_name")
+	unknown_encodings = face_recognition.face_encodings(unknown_image)[0]
+	return unknown_encodings
 
 # Compare the face with the given list of encodings and return the possible
-# entry in the known encodings.
-def compareFaces():
-	
-#Compares the faces of know and unknown images 
-#if the faces were identical returns [True]	
-#else returns [False]
-	
-	results = face_recognition.compare_faces([known_encodings], unknown_encodings)
-	
-	return results
-	
-
-	
-
-	
+# entry in the known encodings. Return -1 if nothing's been found.
+def compare(known_encodings, unknown_encodings):
+	results = fr.compare_faces([known_encodings], unknown_encodings, tolerance = tol)
+	for m in range(0, len(results)):
+		if results[m]:
+			return m
+	return -1
